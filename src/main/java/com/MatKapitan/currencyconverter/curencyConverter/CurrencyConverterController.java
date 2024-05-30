e9f0da1d7e30b34f6acb83d2de6c462e8a75a4b0
MatKapitan <matija.kapitan1@gmail.com>
2024-05-30 23:49:27 +0200

    feat added open api

--- src/main/java/com/MatKapitan/currencyconverter/curencyConverter/CurrencyConverterController.java
+++ src/main/java/com/MatKapitan/currencyconverter/curencyConverter/CurrencyConverterController.java
@@ -1,12 +1,12 @@
 package com.MatKapitan.currencyconverter.curencyConverter;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.EnumSet;
@@ -14,12 +14,14 @@ import java.util.EnumSet;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/currency-converter")
+@Tag(name = "Currency converter")
 public class CurrencyConverterController {
     
     private final CurrencyConverterService currencyConverterService;
 
 
 
+    @Operation(description = "Returns target currency amount, if not provided target currency is EUR")
     @GetMapping("/")
     public ResponseEntity<BigDecimal> currencyConverter(@RequestParam(name = "sourceCurrency") ECurrency sourceCurrency,
                                                         @RequestParam(name = "sourceAmount") BigDecimal sourceAmount,
@@ -28,12 +30,12 @@ public class CurrencyConverterController {
         return new ResponseEntity<>(convert, HttpStatus.OK);
     }
 
+    @Operation(description = "Returns list of all supported currencies")
     @GetMapping("/all")
     public ResponseEntity<EnumSet<ECurrency>> getAllCurrencies(){
         EnumSet<ECurrency> allCurrency = currencyConverterService.getAllCurrencies();
         return new ResponseEntity<>(allCurrency, HttpStatus.OK);
     }
-
 
     @GetMapping("/test")
     public void test(){
